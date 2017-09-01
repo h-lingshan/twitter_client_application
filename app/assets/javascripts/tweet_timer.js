78e400fd6d7ec6788e89ab76b2a04c7e29657479
ReeceHsu <viewsoic521@gmail.com>
2017-09-01 11:43:11 +0900

    取得されたタイムラインはパーシャルに反映されてないバグを修正

--- app/assets/javascripts/tweet_timer.js
+++ app/assets/javascripts/tweet_timer.js
@@ -1,22 +1,28 @@
 $(document).ready(function() {
-  $('#btn_new_tweet').hide();
-  setInterval(refreshPartial,60000); //linetimeはRate Limitedにひっかかるので20秒ごとにapiにアクセスすることに60秒に変える
-  $('#btn_new_tweet').click(function(){
-    $.ajax({
-      url: "/time_line",
-    })
     $('#btn_new_tweet').hide();
-  })
+    setInterval(refreshPartial, 60000); //linetimeはRate Limitedにひっかかるので20秒ごとにapiにアクセスすることに60秒に変える
+    $('#btn_new_tweet').click(function() {
+        $.ajax({
+            url: "/time_line",
+            dataType: 'script',
+            type: "GET",
+            data: { max_id: $('#new_max_id').val() },
+        }).done(function(data, textStatus, jqXHR){
+            $('#max_id').val(('#new_max_id').val());
+        })
+        $('#btn_new_tweet').hide();
+    })
 })
-function refreshPartial(){
-  $.ajax({
-      url: "/time_line_max_id",
-      dataType: 'json',
-      success : function(data){
-        if(data.max_id != $('.max_id').val())
-        {
-           $('#btn_new_tweet').show();
+
+function refreshPartial() {
+    $.ajax({
+        url: "/time_line_max_id",
+        dataType: 'json',
+        success: function(data) {
+            if (data.max_id != $('#max_id').val()) {
+                $('#new_max_id').val(data.max_id);
+                $('#btn_new_tweet').show();
+            }
         }
-      }
-  })
+    })
 }
